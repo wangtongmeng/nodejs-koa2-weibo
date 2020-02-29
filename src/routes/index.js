da58f65bd0cc5a79a26117e11ff7caaf4cf292b8
wangtongmeng <tongmengwang@163.com>
2020-02-29 16:52:08 +0800

    feat: debug 配置

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  // console.log('before debugger')
+  // debugger
+  // console.log('after debugger')
   await ctx.render('index', {
     title: 'Hello Koa 2!',
     msg: '你好',
