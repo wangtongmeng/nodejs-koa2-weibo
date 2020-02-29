01906adfc7be665349afde55dddcfa080cd4c9ac
wangtongmeng <tongmengwang@163.com>
2020-02-29 19:36:23 +0800

    feat: 404和错误页路由

--- src/app.js
+++ src/app.js
@@ -9,12 +9,21 @@ const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
 
 const { REDIS_CONF } = require('./conf/db')
+const { isProd } = require('./utils/env')
 
+// 路由 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const errorViewRoute = require('./routes/view/error')
 
 // error handler 页面上显示错误信息
-onerror(app)
+let onerrorConf = {}
+if (isProd) {
+  onerrorConf = {
+    redirect: '/error'
+  }
+}
+onerror(app, onerrorConf)
 
 // middlewares
 // 解析 post 请求数据
@@ -46,17 +55,11 @@ app.use(session({
   })
 }))
 
-// logger 只是一个中间件的演示
-// app.use(async (ctx, next) => {
-//   const start = new Date()
-//   await next()
-//   const ms = new Date() - start
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-// })
-
 // routes 注册路由
+
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRoute.routes(), errorViewRoute.allowedMethods()) // 404路由注册到最下面
 
 // error-handling 控制台打印错误信息
 app.on('error', (err, ctx) => {
